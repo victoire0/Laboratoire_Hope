15eb05a5a02f641a7d79d23fe461e4f18e527e14
victoire0 <aminikayembe1@gmail.com>
2024-08-09 02:50:00 -0700

    initial

---
+++ src/hope/Hope.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hope;
+
+import vue.Formprincipal;
+
+/**
+ *
+ * @author HomePC
+ */
+public class Hope {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Formprincipal p = new Formprincipal();
+        p.setTitle("GESTION DES RESULTAT D'UN LABORATOIRE D'HOPITAL");
+        p.setSize(800, 600);
+        p.setVisible(true);
+    }
+    
+}
